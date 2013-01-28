4b48824f623c11ddc8239fd717c0181c216b19ad
Pranav Ravichandran <me@onloop.net>
2013-01-28 19:19:07 +0530

    Namespaces.

--- faces.py
+++ faces.py
@@ -1,28 +1,27 @@
-from cv import *
-from PIL import *
+import cv
 from PIL import Image, ImageFilter
 from random import choice
 from os import remove
 
-targetImage = LoadImage('sample.jpg')
-haarCascade = Load('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
-storage = CreateMemStorage()
-Faces = HaarDetectObjects(targetImage, haarCascade, storage, min_neighbors = 3)
+targetImage = cv.LoadImage('sample.jpg')
+haarCascade = cv.Load('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
+storage = cv.CreateMemStorage()
+Faces = cv.HaarDetectObjects(targetImage, haarCascade, storage, min_neighbors = 3)
 count = 0
 coordsList = []
 
 if Faces:
 	for face in Faces:
-		#Rectangle(targetImage, (face[0][0], face[0][1]), (face[0][0] + face[0][2], face[0][1] + face[0][3]), RGB(155, 255, 25), 2)
-		SetImageROI(targetImage, face[0])
-		regionImage = CreateImage(GetSize(targetImage), targetImage.depth, targetImage.nChannels)
-		Copy(targetImage, regionImage, None)
-		ResetImageROI(targetImage)
-		SetImageROI(targetImage, Faces[count][0])
-		targetRegion = CreateImage(GetSize(targetImage), targetImage.depth, targetImage.nChannels)
-		Copy(targetImage, targetRegion, None)
-		ResetImageROI(targetImage)
-		SaveImage(str(count) + ".jpg", targetRegion)
+		#cv.Rectangle(targetImage, (face[0][0], face[0][1]), (face[0][0] + face[0][2], face[0][1] + face[0][3]), RGB(155, 255, 25), 2)
+		cv.SetImageROI(targetImage, face[0])
+		regionImage = cv.CreateImage(cv.GetSize(targetImage), targetImage.depth, targetImage.nChannels)
+		cv.Copy(targetImage, regionImage, None)
+		cv.ResetImageROI(targetImage)
+		cv.SetImageROI(targetImage, Faces[count][0])
+		targetRegion = cv.CreateImage(cv.GetSize(targetImage), targetImage.depth, targetImage.nChannels)
+		cv.Copy(targetImage, targetRegion, None)
+		cv.ResetImageROI(targetImage)
+		cv.SaveImage(str(count) + ".jpg", targetRegion)
 		count += 1
 		coordsList.append((face[0][0], face[0][1]))
 
@@ -39,4 +38,4 @@ parentImage.save("swapped.jpg", "JPEG")
 
 #NamedWindow('Face Detection', CV_WINDOW_NORMAL)
 #ShowImage('Face Detection', targetImage) 
-WaitKey()
+#cv.WaitKey()
